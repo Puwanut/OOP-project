c7119c24effc0719fed02857f9b02d31161d26df
62070021 <eth_9tynine@hotmail.com>
2020-11-14 14:41:29 +0700

    Create Connect.java

---
+++ oop-project/src/Connect.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kanasin
+ */
+import java.sql.*;
+public class Connect {
+    
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/dbmysql";
+            Connection con = DriverManager.getConnection(url, "root", "");
+            return con;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;    
+}
+}
