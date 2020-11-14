6126f557d13f3ea3b657b5c7c5dcbbf0c2709c5a
62070021 <eth_9tynine@hotmail.com>
2020-11-14 15:05:57 +0700

    earth

--- oop-project/src/Connect.java
+++ oop-project/src/Connect.java
@@ -14,7 +14,7 @@ public class Connect {
     public static Connection ConnectDB(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost/dbmysql";
+            String url = "jdbc:mysql://localhost/mdb";
             Connection con = DriverManager.getConnection(url, "root", "");
             return con;
         }catch(Exception e){
